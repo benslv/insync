a06f39cdcf64e324ab0def170e6f4bec9f38d00b
benslv <bensilverman10@gmail.com>
2023-01-08 17:18:58 +0000

    feat: animate circles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -184,7 +184,7 @@ export default function Index() {
 					</>
 				) : null}
 			</div>
-			<div className="relative hidden w-full h-full overflow-hidden sm:block">
+			<div className="relative items-center hidden w-full h-full overflow-hidden sm:flex sm:justify-end">
 				<BackgroundCircles />
 			</div>
 		</div>
@@ -217,12 +217,46 @@ function ProfileImage({
 
 function BackgroundCircles() {
 	return (
-		<div className="absolute top-1/2 -translate-y-1/2 left-1/4 flex items-center justify-center w-[1527px] border-2 border-green-500 rounded-full h-[1527px]">
-			<div className="relative flex items-center justify-center w-[1221px] border-2 border-green-500 rounded-full h-[1221px]">
-				<div className="relative flex items-center justify-center w-[917px] border-2 border-green-500 rounded-full h-[917px]">
-					<div className="w-[611px] border-2 border-green-500 rounded-full h-[611px]"></div>
-				</div>
-			</div>
+		<div className="flex items-center justify-center">
+			<motion.div
+				animate={{ scale: [1, 1.15, 1] }}
+				transition={{
+					repeat: Infinity,
+					ease: "easeInOut",
+					duration: 5,
+					delay: 0.3,
+				}}
+				className="absolute items-center justify-center w-[1527px] flex-0 border-2 border-green-500 rounded-full h-[1527px]"
+			></motion.div>
+			<motion.div
+				animate={{ scale: [1, 1.2, 1] }}
+				transition={{
+					repeat: Infinity,
+					ease: "easeInOut",
+					duration: 5,
+					delay: 0.2,
+				}}
+				className="absolute items-center justify-center w-[1221px] flex-0 border-2 border-green-500 rounded-full h-[1221px]"
+			></motion.div>
+			<motion.div
+				animate={{ scale: [1, 1.3, 1] }}
+				transition={{
+					repeat: Infinity,
+					ease: "easeInOut",
+					duration: 5,
+					delay: 0.1,
+				}}
+				className="absolute items-center justify-center w-[917px] flex-0 border-2 border-green-500 rounded-full h-[917px]"
+			></motion.div>
+			<motion.div
+				animate={{ scale: [1, 1.4, 1] }}
+				transition={{
+					repeat: Infinity,
+					ease: "easeInOut",
+					duration: 5,
+				}}
+				className="absolute w-[611px] border-2 border-green-500 rounded-full flex-0 h-[611px]"
+			></motion.div>
 		</div>
 	);
 }
