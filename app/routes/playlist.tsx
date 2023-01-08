82feae2e6e94c58a0720f5ec8d1539a08e2b10ee
benslv <bensilverman10@gmail.com>
2023-01-08 15:47:08 +0000

    feat: playlist card

--- app/routes/playlist.tsx
+++ app/routes/playlist.tsx
@@ -35,11 +35,50 @@ export default function GeneratePage() {
 		))
 		.reduce((prev, curr) => [prev, ", ", curr]);
 
+	const {
+		id: playlistId,
+		name: playlistName,
+		owner: { display_name: username },
+		images,
+	} = playlist;
+
+	const playlistUrl = `https://open.spotify.com/playlist/${playlistId}`;
+
+	const playlistImageUrl =
+		images?.[1]?.url || images?.[0].url || "/images/cover.png";
+
 	return (
-		<div className="flex items-center justify-center w-full h-full">
-			<h1 className="mb-2 text-3xl sm:text-4xl">
-				Your playlist with {artists} + others
-			</h1>
+		<div className="flex items-center justify-center w-full h-full px-4">
+			<div className="flex flex-col items-center justify-center max-w-xl gap-y-8">
+				<h1 className="mb-2 text-3xl text-center sm:text-4xl">
+					Your playlist with {artists} + others
+				</h1>
+				<div className="flex flex-col items-center px-8 py-8 space-y-4 border rounded-lg bg-neutral-800 border-neutral-700">
+					<img
+						src={playlistImageUrl}
+						alt={`Playlist for ${username}`}
+						className="rounded-md w-52 h-52 bg-neutral-400"
+						height={208}
+						width={208}
+					/>
+					<div className="text-center">
+						<h2 className="text-2xl">
+							{playlistName || "insync mixtape"}
+						</h2>
+						<p className="text-sm text-neutral-300">
+							For {username || "you"}
+						</p>
+					</div>
+					<a
+						href={playlistUrl}
+						target="_blank"
+						rel="noreferrer"
+						className="px-4 py-2 text-sm font-bold uppercase transition-colors bg-green-500 rounded-full hover:bg-green-400 text-neutral-900 w-max"
+					>
+						Open
+					</a>
+				</div>
+			</div>
 		</div>
 	);
 }
