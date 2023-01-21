2de19a1b7f84274603b51407e0e1aeacbad1441b
benslv <bensilverman10@gmail.com>
2023-01-21 15:59:02 +0000

    Improve outlet sizing

--- app/routes/_layout.tsx
+++ app/routes/_layout.tsx
@@ -7,10 +7,10 @@ export default function GenerateIndex() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full h-full gap-y-4">
 			<div className="flex flex-col items-center gap-y-2">
-				<p className="text-sm text-neutral-500">
+				<p className="text-sm text-neutral-400">
 					Choose generation method:
 				</p>
-				<div className="flex p-1 border rounded-lg w-max bg-neutral-900 border-neutral-700 gap-x-1">
+				<div className="flex p-1 border rounded-lg w-max bg-neutral-900 border-neutral-600 gap-x-1">
 					<Link
 						to="./basic"
 						className={`px-2 align-baseline py-1 rounded-md hover:bg-neutral-800 border transition-colors ${
@@ -33,11 +33,10 @@ export default function GenerateIndex() {
 					</Link>
 				</div>
 			</div>
-			<div className="flex flex-col w-full max-w-4xl p-6 border rounded-xl border-neutral-600 gap-y-4">
+			<div className="flex flex-col max-w-4xl p-6 border w-max rounded-xl border-neutral-600 gap-y-4">
 				<div className="flex flex-col gap-y-2">
 					<h1 className="text-5xl tracking-tighter">Generate</h1>
 
-					<div className="flex items-center gap-x-2"></div>
 					<Outlet />
 				</div>
 			</div>
