a3ee770c2ebcd4dc07e623d0136c0d04e8efc188
benslv <bensilverman10@gmail.com>
2023-02-05 21:10:41 +0000

    feat: implement artist chips and selection

--- app/routes/_layout.studio.tsx
+++ app/routes/_layout.studio.tsx
@@ -1,6 +1,9 @@
 import type { ActionArgs } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
 import { SpotifyWebApi } from "@thomasngrlt/spotify-web-api-ts";
+import type { Artist } from "@thomasngrlt/spotify-web-api-ts/types/types/SpotifyObjects";
+import { useState } from "react";
 import { z } from "zod";
 
 import { Label } from "~/components/Label";
@@ -18,6 +21,10 @@ const generateOptions = z.object({
 });
 
 type GenerateOptions = z.infer<typeof generateOptions>;
+
+export async function loader({ request }: ActionArgs) {
+	return json({ artists });
+}
 
 export async function action({ request }: ActionArgs) {
 	const formData = await request.formData();
@@ -78,68 +85,96 @@ export async function action({ request }: ActionArgs) {
 }
 
 export default function StudioPage() {
+	const { artists } = useLoaderData<typeof loader>();
+	const [selectedArtists, setSelectedArtists] = useState<string[]>([]);
+
+	const handleChipClick = (id: string) => {
+		if (selectedArtists.includes(id)) {
+			return setSelectedArtists((prev) =>
+				prev.filter((selectedId) => selectedId !== id)
+			);
+		}
+
+		if (selectedArtists.length === 5) return;
+
+		return setSelectedArtists((prev) => [...prev, id]);
+	};
+
+	const artistChips = artists.map(({ name, images, id }) => (
+		<ArtistChip
+			key={id}
+			image={images![0].url ?? ""}
+			text={name!}
+			onClick={() => handleChipClick(id!)}
+			selected={selectedArtists.includes(id!)}
+		/>
+	));
+
 	return (
-		<Form
-			method="post"
-			className="flex flex-col items-center w-full gap-y-4"
-		>
-			<div>
-				<Label htmlFor="name">Playlist Name</Label>
-				<TextInput
-					id="name"
-					name="name"
-					placeholder="insync mixtape"
-					className="w-full mt-1"
-				/>
-			</div>
-			<div className="flex items-center gap-x-4">
-				<Label>How many tracks? (max. 100)</Label>
-				<NumberInput
-					className="w-16 hide-spinner"
-					name="track_count"
-					min={1}
-					max={100}
-				/>
-			</div>
-			<RangeGroup label="Tempo (BPM)" leftText="30" rightText="300">
-				<RangeSlider
-					name="tempo"
-					id="tempo"
-					min={30}
-					max={300}
-					className="w-[350px]"
-				/>
-			</RangeGroup>
-			<RangeGroup
-				label="Popularity"
-				leftText="Obscure finds"
-				rightText="Chart toppers"
+		<div className="flex">
+			<div className="flex flex-wrap gap-2 h-min">{artistChips}</div>
+			<Form
+				method="post"
+				className="flex flex-col items-center w-full gap-y-4"
 			>
-				<RangeSlider
-					name="popularity"
-					id="popularity"
-					min={0}
-					max={100}
-					step={1}
-					className="w-[350px]"
-				/>
-			</RangeGroup>
-			<RangeGroup
-				label="Energy"
-				leftText="Chilling out"
-				rightText="Ramping up"
-			>
-				<RangeSlider
-					name="energy"
-					id="energy"
-					min={0}
-					max={1}
-					step={0.01}
-					className="w-[350px]"
-				/>
-			</RangeGroup>
-			<button type="submit">Create</button>
-		</Form>
+				<div>
+					<Label htmlFor="name">Playlist Name</Label>
+					<TextInput
+						id="name"
+						name="name"
+						placeholder="insync mixtape"
+						className="w-full mt-1"
+					/>
+				</div>
+				<div className="flex items-center gap-x-4">
+					<Label>How many tracks? (max. 100)</Label>
+					<NumberInput
+						className="w-16 hide-spinner"
+						name="track_count"
+						min={1}
+						max={100}
+					/>
+				</div>
+				<RangeGroup label="Tempo (BPM)" leftText="30" rightText="300">
+					<RangeSlider
+						name="tempo"
+						id="tempo"
+						min={30}
+						max={300}
+						className="w-[350px]"
+					/>
+				</RangeGroup>
+				<RangeGroup
+					label="Popularity"
+					leftText="Obscure finds"
+					rightText="Chart toppers"
+				>
+					<RangeSlider
+						name="popularity"
+						id="popularity"
+						min={0}
+						max={100}
+						step={1}
+						className="w-[350px]"
+					/>
+				</RangeGroup>
+				<RangeGroup
+					label="Energy"
+					leftText="Chilling out"
+					rightText="Ramping up"
+				>
+					<RangeSlider
+						name="energy"
+						id="energy"
+						min={0}
+						max={1}
+						step={0.01}
+						className="w-[350px]"
+					/>
+				</RangeGroup>
+				<button type="submit">Create</button>
+			</Form>
+		</div>
 	);
 }
 
@@ -166,3 +201,109 @@ function RangeGroup({ label, leftText, rightText, children }: RangeGroupProps) {
 		</div>
 	);
 }
+
+type ArtistChipProps = {
+	image: string;
+	text: string;
+	selected: boolean;
+	onClick: () => void;
+};
+
+function ArtistChip({ image, text, onClick, selected }: ArtistChipProps) {
+	return (
+		<div
+			onClick={onClick}
+			className={`flex items-center py-1 pl-1 pr-2 transition-colors border-2 bg-neutral-800 hover:bg-neutral-700 rounded-full gap-x-2 hover:cursor-pointer w-max h-min
+			${
+				selected
+					? "border-green-600 hover:border-green-500"
+					: "border-neutral-600 hover:border-neutral-500"
+			}
+			`}
+		>
+			<img
+				src={image}
+				alt={`${text} icon`}
+				width={20}
+				height={20}
+				className="rounded-full"
+			/>
+			<p
+				className={`text-sm transition-colors  ${
+					selected ? "text-white" : "text-neutral-400"
+				}`}
+			>
+				{text}
+			</p>
+		</div>
+	);
+}
+
+const artists: Partial<Artist>[] = [
+	{
+		name: "Tennyson",
+		id: "1",
+		images: [
+			{
+				url: "https://d1fdloi71mui9q.cloudfront.net/irzKPVGwQpqsdV53mjP5_AD1SoivTLfVycUAm",
+				height: 100,
+				width: 100,
+			},
+		],
+	},
+	{
+		name: "Tennyson",
+		id: "2",
+		images: [
+			{
+				url: "https://d1fdloi71mui9q.cloudfront.net/irzKPVGwQpqsdV53mjP5_AD1SoivTLfVycUAm",
+				height: 100,
+				width: 100,
+			},
+		],
+	},
+	{
+		name: "Tennyson",
+		id: "3",
+		images: [
+			{
+				url: "https://d1fdloi71mui9q.cloudfront.net/irzKPVGwQpqsdV53mjP5_AD1SoivTLfVycUAm",
+				height: 100,
+				width: 100,
+			},
+		],
+	},
+	{
+		name: "Tennyson",
+		id: "4",
+		images: [
+			{
+				url: "https://d1fdloi71mui9q.cloudfront.net/irzKPVGwQpqsdV53mjP5_AD1SoivTLfVycUAm",
+				height: 100,
+				width: 100,
+			},
+		],
+	},
+	{
+		name: "Tennyson",
+		id: "5",
+		images: [
+			{
+				url: "https://d1fdloi71mui9q.cloudfront.net/irzKPVGwQpqsdV53mjP5_AD1SoivTLfVycUAm",
+				height: 100,
+				width: 100,
+			},
+		],
+	},
+	{
+		name: "Tennyson",
+		id: "6",
+		images: [
+			{
+				url: "https://d1fdloi71mui9q.cloudfront.net/irzKPVGwQpqsdV53mjP5_AD1SoivTLfVycUAm",
+				height: 100,
+				width: 100,
+			},
+		],
+	},
+];
