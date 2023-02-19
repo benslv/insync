88b7883cabb2dda90e2c2cbaf33961c95a45f88b
benslv <bensilverman10@gmail.com>
2023-02-19 11:32:48 +0000

    feat: add page stepper

--- app/components/OnboardingModal.tsx
+++ app/components/OnboardingModal.tsx
@@ -1,7 +1,15 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Plus } from "iconoir-react";
+import { ArrowLeft, ArrowRight, Plus } from "iconoir-react";
+import { useState } from "react";
 
 export function OnboardingModal() {
+	const [currentPage, setCurrentPage] = useState(0);
+
+	const nextPage = () => setCurrentPage((page) => page + 1);
+	const prevPage = () => setCurrentPage((page) => page - 1);
+
+	const numPages = 3;
+
 	return (
 		<Dialog.Root>
 			<Dialog.Trigger asChild>
@@ -9,22 +17,65 @@ export function OnboardingModal() {
 			</Dialog.Trigger>
 			<Dialog.Portal forceMount>
 				<Dialog.Overlay className="fixed z-10 h-screen w-screen bg-black/50" />
-				<Dialog.Content className="fixed top-1/2 left-1/2 z-10 min-h-[376px] w-10/12 max-w-[640px] -translate-x-1/2 -translate-y-1/2 rounded-xl border border-neutral-700 bg-neutral-800 p-6 focus:outline-none sm:h-[376px]">
-					<StepOne />
-					<StepTwo />
+				<Dialog.Content className="fixed top-1/2 left-1/2 z-10 h-max w-10/12 max-w-[640px] -translate-x-1/2 -translate-y-1/2 items-center rounded-xl border border-neutral-700 bg-neutral-800 p-6 focus:outline-none sm:h-[376px] sm:min-h-[376px]">
+					<div className="relative flex h-full w-full items-center">
+						<button
+							type="button"
+							onClick={prevPage}
+							className={`rounded-xl p-2 transition-colors hover:bg-neutral-600 focus-visible:bg-neutral-600 focus-visible:outline-none ${
+								currentPage === 0 ? "invisible" : "visible"
+							}`}>
+							<ArrowLeft />
+						</button>
+						<div className="flex h-full flex-grow flex-col items-center gap-y-8">
+							<StepOne />
+							<Pager pageCount={numPages} currentPage={currentPage} />
+						</div>
+						<button
+							type="button"
+							onClick={nextPage}
+							className={`rounded-xl p-2 transition-colors hover:bg-neutral-600 focus-visible:bg-neutral-600 focus-visible:outline-none ${
+								currentPage === numPages - 1 ? "invisible" : "visible"
+							}`}>
+							<ArrowRight />
+						</button>
+					</div>
 				</Dialog.Content>
 			</Dialog.Portal>
 		</Dialog.Root>
 	);
 }
 
+function Pager({
+	pageCount,
+	currentPage,
+}: {
+	pageCount: number;
+	currentPage: number;
+}) {
+	return (
+		<div className="flex w-max gap-x-2">
+			{Array(pageCount)
+				.fill(undefined)
+				.map((_, i) => (
+					<div
+						key={i}
+						className={`h-1 w-6 rounded-full transition-colors ${
+							currentPage === i ? "bg-neutral-500" : "bg-neutral-700"
+						}`}
+					/>
+				))}
+		</div>
+	);
+}
+
 function StepOne() {
 	return (
-		<div className="relative flex h-max flex-col items-center">
+		<div className="relative flex flex-grow flex-col items-center gap-y-4">
 			<h1 className="text-center text-neutral-300">
 				Connect your Spotify account with insync
 			</h1>
-			<div className="flex w-3/4 flex-grow flex-col items-center justify-center gap-y-4 gap-x-4 sm:flex-row sm:items-center sm:justify-between">
+			<div className="flex flex-grow flex-col items-center justify-center gap-y-4 gap-x-4 sm:flex-row sm:items-center sm:justify-between">
 				<h2 className="text-5xl">insync</h2>
 				<Plus height={90} width={90} strokeWidth={0.2} />
 				<img
@@ -44,7 +95,7 @@ function StepTwo() {
 			<h1 className="text-center text-neutral-300">
 				Selection your preferred generation model
 			</h1>
-			<div className="flex w-3/4 max-w-[396px] flex-grow flex-col items-center justify-center gap-y-4 gap-x-4 sm:flex-row sm:items-center sm:justify-evenly">
+			<div className="flex flex-grow flex-col items-center justify-center gap-y-4 gap-x-4 sm:flex-row sm:items-center sm:justify-evenly">
 				<div className="flex flex-col items-center gap-y-4">
 					<h2 className="text-3xl">Basic</h2>
 					<ul className="list-disc">
