3ea7fcc857d9ba169c7218968f4fc3590ecee2c6
Ben <bensilverman10@gmail.com>
2023-02-27 11:51:51 +0000

    feat: add cookie secret (#22)

--- app/sessions.ts
+++ app/sessions.ts
@@ -1,9 +1,12 @@
 import { createCookieSessionStorage } from "@remix-run/node";
+import { z } from "zod";
+
+const cookieSecret = z.string().parse(process.env.COOKIE_SECRET);
 
 export const { getSession, commitSession, destroySession } =
 	createCookieSessionStorage({
 		cookie: {
 			name: "__session",
-			secrets: [],
+			secrets: [cookieSecret],
 		},
 	});
