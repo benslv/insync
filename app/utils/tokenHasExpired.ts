9b6ed1cde982f644bacb03fefce28338b2e55af2
benslv <bensilverman10@gmail.com>
2023-03-04 18:04:51 +0000

    improve tokenHasExpired

--- app/utils/tokenHasExpired.ts
+++ app/utils/tokenHasExpired.ts
@@ -4,6 +4,8 @@ import { isAfter, parseISO } from "date-fns";
 export function tokenHasExpired(session: Session) {
 	const expiryDate = session.get("expiry_date");
 
+	if (!expiryDate) return true;
+
 	const now = new Date();
 	return isAfter(now, parseISO(expiryDate));
 }
